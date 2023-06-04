24764cb95aebed0e9fc171a6deec96c40f7f11cc
Egor Mikhalev <rasengan21196@gmail.com>
2023-06-04 15:45:15 +0300

    [expect/actual] created fabric for sqldelight driver

---
+++ shared/src/commonMain/kotlin/ru/maybolz/noteskmm/data/local/DatabaseDriverFactory.kt
@@ -0,0 +1,11 @@
+package ru.maybolz.noteskmm.data.local
+
+import com.squareup.sqldelight.db.SqlDriver
+
+/** expect определяет контракт API для общего кода
+* чтобы использовать одну абстракция,на разных платформах (expect),
+ * но подставлять разную реализацию (actual)
+ * */
+expect class DatabaseDriverFactory {
+    fun createDriver(): SqlDriver
+}
